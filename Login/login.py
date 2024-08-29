ca6b3c30c5bc11a2c95c6d6e26207687e3527c19
adeena-syeda <adeenasyedax@gmail.com>
2024-08-29 10:42:31 +0500

    New login.py file

--- Login/login.py
+++ Login/login.py
@@ -1,4 +1,12 @@
+"""for loop is created"""
 for i in range(1,11):
     print("Login sucessfull!")
 
 print("DONE :)")
+
+data = {'a':0,
+        'b':1,
+        'c':2,
+        'd':3,}
+
+print("live view complete")
